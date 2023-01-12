5b6024227f7e5080c33eb9281444c374f4b1c38e
Ascoding2022 <119110065+Ascoding2022@users.noreply.github.com>
2023-01-12 17:51:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -16,18 +16,6 @@ class ScratchEase {
             TWO: {
               type: Scratch.ArgumentType.STRING,
               defaultValue: ''
-        
-            opcode: 'strictlyLessorEquals'
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: '[ONE] is less than or equals [TWO]',
-            arguments: {
-              ONE: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: ''
-              },
-              TWO: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: ''
               }
           }
         }
@@ -37,10 +25,6 @@ class ScratchEase {
   strictlyGreaterorEquals(args) {
     // Note strict equality: Inputs must match exactly: in type, case, etc.
     return args.ONE === args.TWO || args.ONE > args.TWO;
-  }
-  strictlyLessorEquals(args) {
-    // Note strict equality: Inputs must match exactly: in type, case, etc.
-    return args.ONE === args.TWO || args.ONE < args.TWO;
   }
 }
 Scratch.extensions.register(new ScratchEase());
