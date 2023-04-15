b5818135e1777f71845bf3145957b34e0f1d27bb
Ascoding2022 <119110065+Ascoding2022@users.noreply.github.com>
2023-04-15 16:54:20 +0100

    Update script.js
    Big Updates! (2.0 Pre-Release)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-class ScratchEase {
+class addons {
   getInfo() {
     return {
-      id: 'scratchease', // change this if you make an actual extension!
-      name: 'ScratchEase',
+      id: 'addons', // change this if you make an actual extension!
+      name: 'Addons',
       blocks: [
         {
           opcode: 'equalsorgreater',
@@ -15,7 +15,22 @@ class ScratchEase {
             },
             TWO: {
               type: Scratch.ArgumentType.STRING,
+              defaultValue: '50'
+            }
+          }
+        },
+        {
+          opcode: 'equalsorless',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] is equal or less than [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
               defaultValue: ''
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '50'
             }
           }
         }
@@ -28,5 +43,12 @@ class ScratchEase {
     var argstwo = args.TWO * 1;
     return argsone === argstwo || argsone > argstwo;
   }
+
+  equalsorless(args) {
+    // Note strict equality: Inputs must match exactly: in type, case, etc.
+    var argsone = args.ONE * 1;
+    var argstwo = args.TWO * 1;
+    return argsone === argstwo || argsone < argstwo;
+  }
 }
-Scratch.extensions.register(new ScratchEase());
+Scratch.extensions.register(new Addons());
